40326ceec95a4137d8c2255ddbd9e4e28eb5db79
Sebastian Rydz <rydzseba@gmail.com>
2024-12-26 13:53:38 +0100

    feat: connect with go db (#4)
    * feat: i18n, auth, create and view task  (#1)
    
    * feat: connect front with backend
    
    * feat: remove reduntant files
    
    * fix: typos
    
    * blablblablbala
    
    * feat: uh it works, but on error handling
    
    * pre-commit
    
    ---------
    
    Co-authored-by: HermanPlay <hermanplay1110@gmail.com>

--- src/lib/server/db/utils.ts
+++
@@ -1,9 +0,0 @@
-export async function blobToBase64(blob: Blob): Promise<string> {
-	const buffer = Buffer.from(await blob.arrayBuffer());
-	return buffer.toString('base64');
-}
-
-export function base64ToBlob(base64: string, mimeType: string): Blob {
-	const buffer = Buffer.from(base64, 'base64');
-	return new Blob([buffer], { type: mimeType });
-}
