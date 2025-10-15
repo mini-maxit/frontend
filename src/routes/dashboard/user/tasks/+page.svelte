d135342768c329d9771cea8ff07748b58ac3a68b
TheRealSeber <rydzseba@gmail.com>
2025-10-15 19:36:19 +0200

    fix: tasks stats mocks hidding

--- src/routes/dashboard/user/tasks/+page.svelte
+++ src/routes/dashboard/user/tasks/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  // import TasksStats from '$lib/components/dashboard/tasks/TasksStats.svelte';
+  import TasksStats from '$lib/components/dashboard/tasks/TasksStats.svelte';
   import TaskCard from '$lib/components/dashboard/tasks/TaskCard.svelte';
   import Trophy from '@lucide/svelte/icons/trophy';
 
@@ -65,7 +65,7 @@
   </div>
 
   <!-- Stats Banner -->
-  <!-- <TasksStats /> -->
+  <TasksStats />
 
   <!-- Tasks from Contests -->
   <div class="space-y-6">
