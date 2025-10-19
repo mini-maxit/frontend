e4e442765528586c215410c0b7e0c6a1c92cb98b
TheRealSeber <rydzseba@gmail.com>
2025-10-19 13:23:49 +0200

    feat: code syntax highlighting

--- src/lib/components/dashboard/tasks/task-page/tasks/FilePreview.svelte
+++ src/lib/components/dashboard/tasks/task-page/tasks/FilePreview.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
   import * as Card from '$lib/components/ui/card';
   import * as m from '$lib/paraglide/messages';
+  import hljs from 'highlight.js';
+  import 'highlight.js/styles/mono-blue.css';
 
   interface Props {
     content: string;
   }
 
   let { content }: Props = $props();
+
+  const highlighted = $derived(() => {
+    if (!content) return '';
+    const result = hljs.highlightAuto(content);
+    return result.value;
+  });
 </script>
 
 <Card.Root class="flex-1 overflow-hidden">
@@ -14,6 +22,6 @@
     <Card.Title>{m.task_preview_title()}</Card.Title>
   </Card.Header>
   <Card.Content class="mx-6 overflow-auto px-0">
-    <pre class="rounded-lg p-4 text-xs"><code>{content}</code></pre>
+    <pre class="rounded-lg text-xs"><code>{@html highlighted()}</code></pre>
   </Card.Content>
 </Card.Root>
