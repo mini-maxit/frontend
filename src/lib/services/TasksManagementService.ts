72c2ff36b090759fd10c4f22ad54a6e20102a589
HermanPlay <hermanplay1110@gmail.com>
2026-01-22 10:26:55 +0100

    feat: add sortin

--- src/lib/services/TasksManagementService.ts
+++ src/lib/services/TasksManagementService.ts
@@ -46,7 +46,7 @@ export class TasksManagementService {
   }> {
     try {
       const response = await this.apiClient.get<ApiResponse<PaginatedData<Task>>>({
-        url: '/tasks-management/tasks/created?limit=100'
+        url: '/tasks-management/tasks/created?limit=100&sort=title:asc'
       });
       return { success: true, data: response.data.items, status: 200 };
     } catch (error) {
